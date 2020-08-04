2431c1cd47b94947b499c2d439ca61ed86d440e5
yx <52198457+yx649857051@users.noreply.github.com>
2020-08-04 22:35:10 +0800

    Update 轮播.vue

--- 轮播.vue
+++ 轮播.vue
@@ -49,6 +49,56 @@ export default {
       }
     }
   },
+  mounted() {
+    const date = [
+      {
+        year: '2021',
+        month: ['05', '03', '02', '06', '09']
+      },
+      {
+        year: '2020',
+        month: ['02', '03', '07', '09', '10']
+      }
+    ]
+    // 排序年
+    const sortArr = date.sort((a, b) => Number(a.year) - Number(b.year))
+    sortArr.forEach(v => {
+      // 排序月
+      v.month.sort((a, b) => Number(a) - Number(b))
+      let startM // 记录开始位置
+      let lastM // 记录循环上次数据
+      const monthLen = v.month.length
+      v.month.forEach((cv, ci) => {
+        // 第一项
+        if (ci === 0) {
+          startM = cv
+          lastM = cv
+        }
+        // 不连续
+        if (Number(cv) - Number(lastM) > 1) {
+          if (startM === lastM) {
+            // 单个独立月
+            console.log(`${v.year}-${startM}`)
+          } else {
+            // 到此，之前连续月
+            console.log(`${v.year}-${startM}-${lastM}`)
+          }
+          startM = cv
+        }
+        lastM = cv
+        // 最后一项
+        if (ci === monthLen - 1) {
+          if (startM === lastM) {
+            // 单个独立月
+            console.log(`${v.year}-${startM}`)
+          } else {
+            // 到此，之前连续月
+            console.log(`${v.year}-${startM}-${lastM}`)
+          }
+        }
+      })
+    })
+  },
   methods: {
     handleClickSlide(url) {
       console.log('当前点击索引：', this.banners.findIndex(v => v === url))
